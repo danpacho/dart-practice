04bca8cd17d5e05be58af46f32c38fb4fd177b16
Danpa_cho <danpa725@cau.ac.kr>
2023-02-06 14:43:11 +0900

    feat: update main dart file

--- project/pomotoro/lib/main.dart
+++ project/pomotoro/lib/main.dart
@@ -1,1 +1,21 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:pomotoro/common/style/global_style.dart';
+import 'package:pomotoro/providers/timer_provider.dart';
+import 'package:pomotoro/screens/home_screen.dart';
+
+void main() {
+  runApp(const PomodoroApp());
+}
+
+class PomodoroApp extends StatelessWidget {
+  const PomodoroApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GlobalStyle(
+      widget: TimerProvider(
+        child: HomeScreen(),
+      ),
+    );
+  }
+}
