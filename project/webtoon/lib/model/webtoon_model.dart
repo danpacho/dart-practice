654f835f94e969a7185d850d5ac99f3210373544
Danpa_cho <danpa725@cau.ac.kr>
2023-02-11 19:47:06 +0900

    feat: add webtoon info model and webtoon episodes model

--- project/webtoon/lib/model/webtoon_model.dart
+++ project/webtoon/lib/model/webtoon_model.dart
@@ -15,3 +15,46 @@ class WebtoonModel implements Model<WebtoonModel> {
     return WebtoonModel();
   }
 }
+
+class WebtoonInfoModel implements Model<WebtoonInfoModel> {
+  String title = "", about = "", genre = "", age = "", thumb = "";
+
+  @override
+  void set(JsonMap jsonMap) {
+    age = jsonMap["age"];
+    title = jsonMap["title"];
+    about = jsonMap["about"];
+    genre = jsonMap["genre"];
+    thumb = jsonMap["thumb"];
+  }
+
+  @override
+  WebtoonInfoModel create() {
+    return WebtoonInfoModel();
+  }
+}
+
+class WebtoonEpisodesModel implements Model<WebtoonEpisodesModel> {
+  String id = "", title = "", thumb = "";
+  double rating = 0;
+  DateTime date = DateTime.parse("2000-01-01");
+
+  @override
+  void set(JsonMap jsonMap) {
+    id = jsonMap["id"];
+    title = jsonMap["title"];
+    thumb = jsonMap["thumb"];
+    final dated = DateTime.parse(
+      (jsonMap["date"] as String).split(".").join("-"),
+    );
+    if (dated.runtimeType is DateTime) {
+      date = dated;
+    }
+    rating = double.parse(jsonMap["rating"] as String);
+  }
+
+  @override
+  WebtoonEpisodesModel create() {
+    return WebtoonEpisodesModel();
+  }
+}
